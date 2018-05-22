cdaa95e92a7bda585e1b39045e569cc74344e4d3
rypowell1988 <36467089+rypowell1988@users.noreply.github.com>
2018-05-22 22:48:07 +0100

    Update SetupDEPNotify.sh

--- SetupDEPNotify.sh
+++ SetupDEPNotify.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
+# macOS DEPNotify Configuration Script
+#   Version: 1.0
+#   Author: Ryan Powell
+#   Reference: https://github.com/rypowell1988/mac-management/blob/master/SetupDEPNotify.sh
+#   Note: This script uses parameter 4 in the JSS to define the NTP Server
 
 # SETUP SOME VARIABLES
-	depnDir="/var/tmp/DEPNotify"
+    depnDir="/var/tmp/DEPNotify"
     depnLog="$depnDir/DEPNotify.log"
     current_user=`id -un`
 	
@@ -40,55 +45,55 @@
 
 
 # CONFIGURE HOSTNAME VIA JAMF POLICY TRIGGER
-	echo "Status: Setting Hostname..." >> $depnLog
-	sudo /usr/local/jamf/bin/jamf policy -trigger script_sethostname
+    echo "Status: Setting Hostname..." >> $depnLog
+    sudo /usr/local/jamf/bin/jamf policy -trigger script_sethostname
     sleep 1
 
 # CONFIGURE GLOBAL DEFAULTS VIA JAMF POLICY TRIGGER
-	echo "Status: Configuring defaults settings..." >> $depnLog
-	sudo /usr/local/jamf/bin/jamf policy -trigger script_globaldefaults
-	sleep 1
+    echo "Status: Configuring defaults settings..." >> $depnLog
+    sudo /usr/local/jamf/bin/jamf policy -trigger script_globaldefaults
+    sleep 1
 
 
 # CONFIGURE USER TEMPLATE DEFAULTS VIA JAMF POLICY TRIGGER
-	echo "Status: Configuring user template default settings..." >> $depnLog
-	sudo /usr/local/jamf/bin/jamf policy -trigger script_usertemplatedefaults
+    echo "Status: Configuring user template default settings..." >> $depnLog
+    sudo /usr/local/jamf/bin/jamf policy -trigger script_usertemplatedefaults
     sleep 1
 
 
 # DEPLOYING TREND AV VIA JAMF POLICY TRIGGER
-	echo "Status: Installing - Trend AntiVirus..." >> $depnLog
-	sudo /usr/local/jamf/bin/jamf policy -trigger pkg_trendav
+    echo "Status: Installing - Trend AntiVirus..." >> $depnLog
+    sudo /usr/local/jamf/bin/jamf policy -trigger pkg_trendav
     sleep 1
 
 
 # DEPLOYING AVECTO DEFENDPOINT VIA JAMF POLICY TRIGGER
-	echo "Status: Installing - Avecto DefendPoint..." >> $depnLog
-	sudo /usr/local/jamf/bin/jamf policy -trigger pkg_avecto
+    echo "Status: Installing - Avecto DefendPoint..." >> $depnLog
+    sudo /usr/local/jamf/bin/jamf policy -trigger pkg_avecto
     sleep 1
 
 
 # DEPLOYING NEXTHINK VIA JAMF POLICY TRIGGER
-	echo "Status: Installing - Nexthink..." >> $depnLog
-	sudo /usr/local/jamf/bin/jamf policy -trigger pkg_nexthink
+    echo "Status: Installing - Nexthink..." >> $depnLog
+    sudo /usr/local/jamf/bin/jamf policy -trigger pkg_nexthink
     sleep 1
 
 
 # DEPLOYING MICROSOFT OFFICE VIA JAMF POLICY TRIGGER
-	echo "Status: Installing - Microsoft Office..." >> $depnLog
-	sudo /usr/local/jamf/bin/jamf policy -trigger pkg_msoffice
+    echo "Status: Installing - Microsoft Office..." >> $depnLog
+    sudo /usr/local/jamf/bin/jamf policy -trigger pkg_msoffice
     sleep 1
 
 
-# DEPLOYING UOD WALLPAPERS VIA JAMF POLICY TRIGGER
-	echo "Status: Installing - UoD Wallpapers..." >> $depnLog
-	sudo /usr/local/jamf/bin/jamf policy -trigger pkg_uodwallpapers
+# DEPLOYING UOD SCREENSAVER Images VIA JAMF POLICY TRIGGER
+    echo "Status: Installing - UoD Screensaver..." >> $depnLog
+    sudo /usr/local/jamf/bin/jamf policy -trigger pkg_uodscreensaver
     sleep 1
 
 
 # DEPLOYING SELF SERVICE VIA JAMF POLICY TRIGGER
-	echo "Status: Installing - Self Service App..." >> $depnLog
-	sudo /usr/local/jamf/bin/jamf policy -trigger pkg_selfservice
+    echo "Status: Installing - Self Service App..." >> $depnLog
+    sudo /usr/local/jamf/bin/jamf policy -trigger pkg_selfservice
     sleep 1
 
 
