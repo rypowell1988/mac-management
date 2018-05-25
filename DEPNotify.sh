39838238d225b78043c0d58582b44c4956127c17
rypowell1988 <36467089+rypowell1988@users.noreply.github.com>
2018-05-25 12:27:40 +0100

    Create DEPNotify.sh

---
+++ DEPNotify.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# Hard coded into the DEP Notify package to be executed.  This script is essentially our 'image'
+#   Version: 1.0
+#   Author: Ryan Powell
+#   Reference: https://github.com/rypowell1988/mac-management/blob/master/DEPNotifyPlaylist.sh
+
+
+# Start
+
+exit 0
