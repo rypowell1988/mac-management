7ec00ce1a04f1d7e03abdfbce2eec98dc4f0f67e
rypowell1988 <36467089+rypowell1988@users.noreply.github.com>
2018-05-25 12:31:56 +0100

    Update SetupDEPNotify.sh

--- SetupDEPNotify.sh
+++ SetupDEPNotify.sh
@@ -44,62 +44,9 @@
     caffeinate_pid=$!
 
 
-# CONFIGURE HOSTNAME VIA JAMF POLICY TRIGGER
-    echo "Status: Setting Hostname..." >> $depnLog
-    sudo /usr/local/jamf/bin/jamf policy -trigger script_sethostname
-    sleep 1
-
-# CONFIGURE GLOBAL DEFAULTS VIA JAMF POLICY TRIGGER
-    echo "Status: Configuring defaults settings..." >> $depnLog
-    sudo /usr/local/jamf/bin/jamf policy -trigger script_globaldefaults
-    sleep 1
-
-
-# CONFIGURE USER TEMPLATE DEFAULTS VIA JAMF POLICY TRIGGER
-    echo "Status: Configuring user template default settings..." >> $depnLog
-    sudo /usr/local/jamf/bin/jamf policy -trigger script_usertemplatedefaults
-    sleep 1
-
-
-# DEPLOYING TREND AV VIA JAMF POLICY TRIGGER
-    echo "Status: Installing - Trend AntiVirus..." >> $depnLog
-    sudo /usr/local/jamf/bin/jamf policy -trigger pkg_trendav
-    sleep 1
-
-
-# DEPLOYING AVECTO DEFENDPOINT VIA JAMF POLICY TRIGGER
-    echo "Status: Installing - Avecto DefendPoint..." >> $depnLog
-    sudo /usr/local/jamf/bin/jamf policy -trigger pkg_avecto
-    sleep 1
-
-
-# DEPLOYING NEXTHINK VIA JAMF POLICY TRIGGER
-    echo "Status: Installing - Nexthink..." >> $depnLog
-    sudo /usr/local/jamf/bin/jamf policy -trigger pkg_nexthink
-    sleep 1
-
-
-# DEPLOYING MICROSOFT OFFICE VIA JAMF POLICY TRIGGER
-    echo "Status: Installing - Microsoft Office..." >> $depnLog
-    sudo /usr/local/jamf/bin/jamf policy -trigger pkg_msoffice
-    sleep 1
-
-
-# DEPLOYING UOD SCREENSAVER Images VIA JAMF POLICY TRIGGER
-    echo "Status: Installing - UoD Screensaver..." >> $depnLog
-    sudo /usr/local/jamf/bin/jamf policy -trigger pkg_uodscreensaver
-    sleep 1
-
-
-# DEPLOYING SELF SERVICE VIA JAMF POLICY TRIGGER
-    echo "Status: Installing - Self Service App..." >> $depnLog
-    sudo /usr/local/jamf/bin/jamf policy -trigger pkg_selfservice
-    sleep 1
-
-
-# CHECKING IN WITH JAMF
-    echo "Status: Checking in with Jamf..." >> $depnLog
-    sudo /usr/local/jamf/bin/jamf recon
+# EXECUTE THE DEP NOTIFY PLAYLIST VIA JAMF POLICY TRIGGER
+    echo "Status: Gathering required settings..." >> $depnLog
+    sudo /usr/local/jamf/bin/jamf policy -trigger script_depnotifyplaylist
     sleep 1
 
 
