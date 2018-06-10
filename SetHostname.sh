b278c565e3f4da8c06463956cc1faa0aff2d1ca5
rypowell1988 <36467089+rypowell1988@users.noreply.github.com>
2018-06-10 23:38:14 +0100

    Update SetHostname.sh

--- SetHostname.sh
+++ SetHostname.sh
@@ -4,26 +4,32 @@
 #   Author: Ryan Powell
 #   Reference: https://github.com/rypowell1988/mac-management/blob/master/SetHostname.sh
 
-# Variable to hold the calculated Hostname
-mac_name=""
 
-en0Config=`ifconfig en0` > /dev/null 2>&1
-if [[ $? == 0 ]]; then
-    mac_addr=`ifconfig en0 | awk '/ether/{print $2}'`
-    mac_name=${mac_addr//':'/''}
-else
-    # Grab the System Serial Number
-    hw_info=`system_profiler SPHardwareDataType`
-    hw_serial=`echo "${hw_info}" | grep -i "Serial Number (system):" | awk '{print $4}'`
-    mac_name=$hw_serial
-fi
+# VARIABLE TO HOLD THE GENERATED COMPUTER NAME
+   mac_name=""
 
-# Checking a name has chosen and applying this
-if [[ ! $mac_name == "" ]]; then
-    /usr/sbin/scutil --set HostName ${mac_name} > /dev/null 2>&1
-    /usr/sbin/scutil --set LocalHostName ${mac_name} > /dev/null 2>&1
-    /usr/sbin/scutil --set ComputerName ${mac_name} > /dev/null 2>&1
-    dscacheutil -flushcache > /dev/null 2>&1
-fi
+
+# GENERATE THE NAME
+   # Get the Mac address from adapter en0
+   en0Config=`ifconfig en0` > /dev/null 2>&1
+   if [[ $? == 0 ]]; then
+       mac_addr=`ifconfig en0 | awk '/ether/{print $2}'`
+       mac_name=${mac_addr//':'/''}
+   else
+     # Grab the System Serial Number
+       hw_info=`system_profiler SPHardwareDataType`
+       hw_serial=`echo "${hw_info}" | grep -i "Serial Number (system):" | awk '{print $4}'`
+       mac_name=$hw_serial
+   fi
+
+
+# CHECK A NAME WAS GENERATED AND IF SO, APPLY IT
+   if [[ ! $mac_name == "" ]]; then
+       /usr/sbin/scutil --set HostName ${mac_name} > /dev/null 2>&1
+       /usr/sbin/scutil --set LocalHostName ${mac_name} > /dev/null 2>&1
+       /usr/sbin/scutil --set ComputerName ${mac_name} > /dev/null 2>&1
+       dscacheutil -flushcache > /dev/null 2>&1
+   fi
+
 
 exit 0
