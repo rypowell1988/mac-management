4dcdc3b6e67d85dee9383ebe9bed37179fb4d7d3
rypowell1988 <36467089+rypowell1988@users.noreply.github.com>
2018-06-10 23:41:52 +0100

    Update UserTemplateDefaults.sh

--- UserTemplateDefaults.sh
+++ UserTemplateDefaults.sh
@@ -5,47 +5,50 @@
 #   Reference: https://github.com/rypowell1988/mac-management/blob/master/UserTemplateConfig.sh
 #   Note: This script uses parameter 4 in the JSS to define the Safari Home Page
 
-# Check Parameter have been passed
-if [[ $4 == "" ]]; then
-    exit 1
-fi
 
-# Determine OS version
-os_vers=$(sw_vers -productVersion)
-os_build=$(sw_vers -buildVersion)
+# CHECK PARAMETERS HAVE BEEN PASSED
+   if [[ $4 == "" ]]; then
+      exit 1
+   fi
 
 
-# User template defaults
-for USER_TEMPLATE in "/System/Library/User Template"/*
-	do
-		# Disable first login prompts for Siri, iCloud, Gestures and Diagnostics
-		/usr/bin/defaults write "${USER_TEMPLATE}"/Library/Preferences/com.apple.SetupAssistant DidSeeSiriSetup -bool TRUE
-		/usr/bin/defaults write "${USER_TEMPLATE}"/Library/Preferences/com.apple.SetupAssistant DidSeeSyncSetup -bool TRUE
-		/usr/bin/defaults write "${USER_TEMPLATE}"/Library/Preferences/com.apple.SetupAssistant DidSeeSyncSetup2 -bool TRUE
-		/usr/bin/defaults write "${USER_TEMPLATE}"/Library/Preferences/com.apple.SetupAssistant DidSeeCloudSetup -bool TRUE
-		/usr/bin/defaults write "${USER_TEMPLATE}"/Library/Preferences/com.apple.SetupAssistant DidSeeiCloudLoginForStorageServices -bool TRUE
-		/usr/bin/defaults write "${USER_TEMPLATE}"/Library/Preferences/com.apple.SetupAssistant DidSeeTouchIDSetup -bool TRUE
-		/usr/bin/defaults write "${USER_TEMPLATE}"/Library/Preferences/com.apple.SetupAssistant GestureMovieSeen none
-		/usr/bin/defaults write "${USER_TEMPLATE}"/Library/Preferences/com.apple.SetupAssistant LastSeenCloudProductVersion "${os_vers}"
-		
-		# Enable right click
-		/usr/bin/defaults write "${USER_TEMPLATE}"/Library/Preferences/com.apple.driver.AppleHIDMouse Button2 -int 2
-		/usr/bin/defaults write "${USER_TEMPLATE}"/Library/Preferences/com.apple.driver.AppleBluetoothMultitouch.mouse MouseButtonMode -string TwoButton
-		/usr/bin/defaults write "${USER_TEMPLATE}"/Library/Preferences/com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRightClick -int 1
-		
-		# Default to search current directory
-		/usr/bin/defaults write "${USER_TEMPLATE}"/Library/Preferences/com.apple.finder FXDefaultSearchScope -string "SCcf"
-		
-		# Avoid creating .DS_Store files on network or USB volumes
-		/usr/bin/defaults write "${USER_TEMPLATE}"/Library/Preferences/com.apple.desktopservices DSDontWriteNetworkStores -bool true
-		/usr/bin/defaults write "${USER_TEMPLATE}"/Library/Preferences/com.apple.desktopservices DSDontWriteUSBStores -bool true
-		
-		# Minimise into Dock App Icon
-		/usr/bin/defaults write "${USER_TEMPLATE}"/Library/Preferences/com.apple.dock minimize-to-application -bool true
-		
-		# default Safari homepage
-		/usr/bin/defaults write "${USER_TEMPLATE}"/Library/Preferences/com.apple.Safari HomePage -string "$4"
-		
-	done
-	
+# DETERMINE THE OS VERSION
+   os_vers=$(sw_vers -productVersion)
+   os_build=$(sw_vers -buildVersion)
+
+
+# ITERATE THROUGH EACH USER TEMPLATE AND SET DEFAULT VALUES
+   for USER_TEMPLATE in "/System/Library/User Template"/*
+      do
+         # Disable first login prompts for Siri, iCloud, Gestures and Diagnostics
+         /usr/bin/defaults write "${USER_TEMPLATE}"/Library/Preferences/com.apple.SetupAssistant DidSeeSiriSetup -bool TRUE
+         /usr/bin/defaults write "${USER_TEMPLATE}"/Library/Preferences/com.apple.SetupAssistant DidSeeSyncSetup -bool TRUE
+         /usr/bin/defaults write "${USER_TEMPLATE}"/Library/Preferences/com.apple.SetupAssistant DidSeeSyncSetup2 -bool TRUE
+         /usr/bin/defaults write "${USER_TEMPLATE}"/Library/Preferences/com.apple.SetupAssistant DidSeeCloudSetup -bool TRUE
+         /usr/bin/defaults write "${USER_TEMPLATE}"/Library/Preferences/com.apple.SetupAssistant DidSeeiCloudLoginForStorageServices -bool TRUE
+         /usr/bin/defaults write "${USER_TEMPLATE}"/Library/Preferences/com.apple.SetupAssistant DidSeeTouchIDSetup -bool TRUE
+         /usr/bin/defaults write "${USER_TEMPLATE}"/Library/Preferences/com.apple.SetupAssistant GestureMovieSeen none
+         /usr/bin/defaults write "${USER_TEMPLATE}"/Library/Preferences/com.apple.SetupAssistant LastSeenCloudProductVersion "${os_vers}"
+
+         # Enable right click
+         /usr/bin/defaults write "${USER_TEMPLATE}"/Library/Preferences/com.apple.driver.AppleHIDMouse Button2 -int 2
+         /usr/bin/defaults write "${USER_TEMPLATE}"/Library/Preferences/com.apple.driver.AppleBluetoothMultitouch.mouse MouseButtonMode -string TwoButton
+         /usr/bin/defaults write "${USER_TEMPLATE}"/Library/Preferences/com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRightClick -int 1
+
+         # Default to search current directory
+         /usr/bin/defaults write "${USER_TEMPLATE}"/Library/Preferences/com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+         # Avoid creating .DS_Store files on network or USB volumes
+         /usr/bin/defaults write "${USER_TEMPLATE}"/Library/Preferences/com.apple.desktopservices DSDontWriteNetworkStores -bool true
+         /usr/bin/defaults write "${USER_TEMPLATE}"/Library/Preferences/com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+         # Minimise into Dock App Icon
+         /usr/bin/defaults write "${USER_TEMPLATE}"/Library/Preferences/com.apple.dock minimize-to-application -bool true
+
+         # default Safari homepage
+         /usr/bin/defaults write "${USER_TEMPLATE}"/Library/Preferences/com.apple.Safari HomePage -string "$4"
+
+      done
+
+
 exit 0
