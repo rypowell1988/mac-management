83d94b5ce5b80e00c8b46093f96d6234f886f4bf
rypowell1988 <36467089+rypowell1988@users.noreply.github.com>
2018-06-15 14:17:14 +0100

    Update StartmacOSReInstall.sh

--- StartmacOSReInstall.sh
+++ StartmacOSReInstall.sh
@@ -2,7 +2,7 @@
 # ReInstalls macOS using a pre-deployed application path
 #   Version: 1.0
 #   Author: Ryan Powell
-#   Reference: https://github.com/rypowell1988/mac-management/blob/master/UserTemplateConfig.sh
+#   Reference: https://github.com/rypowell1988/mac-management/blob/master/StartmacOSReInstall.sh
 #   Note: This script uses:
 #       Parameter 4 in the JSS to define if the FS should be wiped (yes - default is no)
 #       Parameter 5 in the JSS to define if the FS should be converted to APFS (yes - default is no)
@@ -10,12 +10,12 @@
 
 
 # THE INITIAL COMMAND PARAMETERS
-commandParams="--agreetolicense"
+   commandParams="--nointeraction --agreetolicense"
 
 
 # CHECK IF ERASING THE FS
    if [[ $4 == "yes" ]]; then
-       commandParams="$commandParams --eraseinstall"
+      commandParams="$commandParams --eraseinstall"
    fi
 
 
@@ -30,11 +30,19 @@ commandParams="--agreetolicense"
 # CHECK AN INTEGER REBOOT DELAY IS SUPPLIED
    if [[ $6 =~ ^[0-9]+$ ]]; then
       commandParams="$commandParams --rebootdelay $6"
-   fi;
+   fi
 
 
 # EXECUTE THE COMMAND
-   sudo "/Applications/Install macOS High Sierra.app/Contents/Resources/startosinstall" "commandParams" &
+   sudo /usr/local/jamf/bin/jamf policy -trigger pkg_uodjamfhelperbranding
+   /Library/Application\ Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper -windowType fs  -icon /CasperImaging.png -description "Setup will begin shortly" -heading "Preparing macOS Installer..." -fullScreenLogo -launchd &
+   echo "Executing command: /Applications/Install macOS High Sierra.app/Contents/Resources/startosinstall $commandParams"
+   sudo "/Applications/Install macOS High Sierra.app/Contents/Resources/startosinstall" $commandParams
+   echo "Response code: $?"
+   
+   
+# INSTALL SHOULD HAVE STARTED WITHIN 15 MINS, IF NOT REBOOT THE MAC SO USER CAN CONTINUE WORKING
+   sudo shutdown -r +15 &
 
 
 exit 0
